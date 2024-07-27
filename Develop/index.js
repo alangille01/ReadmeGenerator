c31673350ad89fe581dc248febe814c5125bde61
Alexis Langille <langille.alexis@gmail.com>
2024-07-27 10:08:51 -0400

    Update Comments for index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,70 +1,69 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
 const fs = require('fs');
 const path = require('path');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
+// Create an array of questions for user input
 const questions = [
-    {
-      type: 'input',
-      name: 'title',
-      message: 'What is the title of your project?'
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'Provide a description of your project:'
-    },
-    {
-      type: 'input',
-      name: 'installation',
-      message: 'Provide installation instructions:'
-    },
-    {
-      type: 'input',
-      name: 'usage',
-      message: 'Provide usage information:'
-    },
-    {
-      type: 'input',
-      name: 'contributing',
-      message: 'Provide contribution guidelines:'
-    },
-    {
-      type: 'input',
-      name: 'tests',
-      message: 'Provide test instructions:'
-    },
-    {
-      type: 'list',
-      name: 'license',
-      message: 'Choose a license for your project:',
-      choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None']
-    },
-    {
-      type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub username:'
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: 'Enter your email address:'
-    }
-  ];
-  
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:'
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Provide installation instructions:'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide usage information:'
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Provide contribution guidelines:'
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide test instructions:'
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None']
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username:'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:'
+  }
+];
 
-// TODO: Create a function to write README file
+// Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFileSync(path.join(process.cwd(), fileName), data);
+  fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
-// TODO: Create a function to initialize app
+// Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then((responses) => {
-        writeToFile('README.md', generateMarkdown(responses));
-    });
+  inquirer.prompt(questions).then((responses) => {
+    writeToFile('README.md', generateMarkdown(responses));
+  });
 }
 
 // Function call to initialize app
