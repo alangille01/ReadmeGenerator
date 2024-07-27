a3b38bf30b8bae6926b2f02d6a342d71456a610b
Alexis Langille <langille.alexis@gmail.com>
2024-07-27 09:57:43 -0400

    Render License Link
    Depending on the license chosen, the apprpriate liscense link will be
    shown in README.

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -11,9 +11,18 @@ function renderLicenseBadge(license) {
   return badges[license];
 }
 
-// TODO: Create a function that returns the license link
+// Function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) return '';
+  const links = {
+    'MIT': 'https://opensource.org/licenses/MIT',
+    'Apache 2.0': 'https://opensource.org/licenses/Apache-2.0',
+    'GPL 3.0': 'https://www.gnu.org/licenses/gpl-3.0',
+    'BSD 3-Clause': 'https://opensource.org/licenses/BSD-3-Clause'
+  };
+  return links[license];
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
